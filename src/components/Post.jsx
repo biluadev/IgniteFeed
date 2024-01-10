a47025760be3999d50e785db0dc410807c7b0e95
biluadev <biluadevcod@gmail.com>
2024-01-10 20:18:49 +0100

    Terminando de estilizar o componente post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,9 @@
 import styles from "./Post.module.css";
 export function Post() {
     return (
-        <article>
+        <article className={styles.post}>
             <header>
-                <div className={styles.post}>
+                <div className={styles.author}>
                     <img className={styles.avatar} src="https://github.com/biluadev.png" />
                     <div className={styles.authorInfo}>
                         <strong>EstevãO Bilua</strong>
@@ -12,14 +12,26 @@ export function Post() {
                 </div>
 
                 <time title="10 de Janeiro às 18:59" dateTime="2024-01-10 18:59:30">Publicado há 1h</time>
-                </header>
+            </header>
 
-                <div className={styles.content}>
-                    <p>Fala galera </p>
-                    <p>Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera </p>
-                    <p><a href="">biluadev.cod/developer</a></p>
-                    <p><a href="">#novoprojeto #nlw</a></p>
-                </div>
+            <div className={styles.content}>
+                <p>Fala galera </p>
+                <p>Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera Fala galera </p>
+                <p><a href="">biluadev.cod/developer</a></p>
+                <p><a href="">#novoprojeto #nlw</a></p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder="Deixe seu comentário!"
+                />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
